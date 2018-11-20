7e235c609f44c96393231ef3f0566d22bb3a0e70
DanielBessaix <danielbessaix@gmail.com>
2018-11-20 20:19:25 +0200

    first commit

---
+++ PlanesInTheRiver/Assets/PlaneInTheRiverAssets/Scripts/ObjectPool.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZenvaVR
+{
+    public class ObjectPool : MonoBehaviour {
+        
+        public GameObject poolPrefab;
+        public GameObject ennemi2;
+        
+        public int initialNum = 10;
+        
+        List<GameObject> pooledObjects;
+        
+        void Awake()
+        {
+            if(pooledObjects == null)
+            {
+                InitPool();
+            }
+        }
+        
+        public void InitPool()
+        {
+            pooledObjects = new List<GameObject>();
+            
+            for (int i = 0; i < initialNum; i++)
+            {
+                CreateRandomEnnemi();
+            }
+        }
+        
+        GameObject CreateObj()
+        {
+            GameObject newObj = Instantiate(poolPrefab);
+            
+            newObj.SetActive(false);
+            
+            pooledObjects.Add(newObj);
+
+            return newObj;
+        }
+
+        GameObject CreateObj2()
+        {
+            GameObject newObj = Instantiate(ennemi2);
+
+            newObj.SetActive(false);
+
+            pooledObjects.Add(newObj);
+
+            return newObj;
+        }
+
+        GameObject CreateRandomEnnemi()
+        {
+            int i = Random.Range(0, 10);
+            if (i < 8)
+            {
+                return CreateObj();
+            }
+            else
+            {
+                return CreateObj2();
+            }
+        }
+
+        public GameObject GetObj()
+        {
+            for(int i = 0; i < pooledObjects.Count; i++) {
+                
+                if(!pooledObjects[i].activeInHierarchy)
+                {
+                    pooledObjects[i].SetActive(true);
+                    
+                    return pooledObjects[i];
+                }
+            }
+            
+            GameObject newObj = CreateRandomEnnemi();
+            
+            newObj.SetActive(true);
+            
+            return newObj;
+        }
+        
+        public List<GameObject> GetAllActive()
+        {
+            List<GameObject> activeObjs;
+            activeObjs = new List<GameObject>();
+            
+            for (int i = 0; i < pooledObjects.Count; i++)
+            {
+                if (pooledObjects[i].activeInHierarchy)
+                {
+                    activeObjs.Add(pooledObjects[i]);
+                }
+            }
+
+            return activeObjs;
+        }
+    }
+}
---
+++ PlanesInTheRiver/Assets/Project/Scripts/Enemy.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public delegate void KillHandler ();
+	public event KillHandler OnKill;
+
+	public GameObject bulletPrefab;
+	public float speed;
+    public float horizontalSpeed;
+	public float bulletSpeed;
+	public float shootingInterval = 6f;
+
+	private float shootingTimer;
+
+	// Use this for initialization
+	void OnEnable () {
+		shootingTimer = Random.Range (0f, shootingInterval);
+        int i = Random.Range(-2, 2);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(i*horizontalSpeed, speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		shootingTimer -= Time.deltaTime;
+  
+        if (shootingTimer <= 0f) {
+            shootingTimer = shootingInterval;
+
+			GameObject bulletInstance = Instantiate (bulletPrefab);
+			bulletInstance.transform.SetParent (transform.parent);
+			bulletInstance.transform.position = transform.position;
+			bulletInstance.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, bulletSpeed);
+			Destroy (bulletInstance, 3f);
+		}
+        
+      
+        
+        if (transform.position.x<= -3)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-horizontalSpeed, speed);
+        }
+        if (transform.position.x >= 3)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(horizontalSpeed, speed);
+        }
+
+
+
+    }
+
+	void OnTriggerEnter2D (Collider2D otherCollider) {
+		if (otherCollider.tag == "PlayerBullet") {
+			gameObject.SetActive (false);
+			Destroy (otherCollider.gameObject);
+
+			if (OnKill != null) {
+				OnKill ();
+			}
+		}
+	}
+}
